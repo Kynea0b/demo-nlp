e4d2ada8cc3db4a6f704cea0028f63ae7c76614b
kokeshiM0chi <frogandfrog009@gmail.com>
2025-02-22 14:02:39 +0900

    feat: first commit

---
+++ cl-tohoku-bert-base-japanese-wrime-8-emotions/README.md
@@ -0,0 +1,31 @@
+# models
+
+## ベースのモデル
+
+### About Dataset
+
+CC-100: 74.3GB
+Wikipedia: 4.9GB
+
+tokenize tool:fugashi,mecab-ipadic-NEologd
+
+参考;
+[BERT base Japanese (unidic-lite with whole word masking, CC-100 and jawiki-20230102)](https://huggingface.co/tohoku-nlp/bert-base-japanese-v3)
+
+## 追加学習用のデータセット
+
+> Ver.1: 80人の筆者から収集した43,200件の投稿に感情強度をラベル付けしました。
+https://github.com/ids-cv/wrime
+
+## 出来上がりモデル
+
+Model repository
+[model](https://huggingface.co/kynea0b/cl-tohoku-bert-base-japanese-v3-wrime-8-emotions)
+
+## resultsには代表的な例
+
+代表例となる結果たち。
+
+## 追加実験
+
+今後予定
---
+++ cl-tohoku-bert-base-japanese-wrime-8-emotions/sample.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import japanize_matplotlib
+import numpy as np
+import pandas as pd
+
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+# sns.set_theme(font='IPAexGothic')
+# 動作確認
+plt.figure(figsize=(5,1))
+plt.title('日本語を表示できるかテスト')
+
+
+
+# https://www.delftstack.com/ja/howto/numpy/numpy-softmax/
+def np_softmax(x):
+    f_x = np.exp(x) / np.sum(np.exp(x))
+    return f_x
+
+def analyze_emotion(text, show_fig=False, ret_prob=False):
+    # 推論モードを有効か
+    model.eval()
+
+    # 入力データ変換 + 推論
+    tokens = tokenizer(text, truncation=True, return_tensors="pt")
+    tokens.to(model.device)
+    preds = model(**tokens)
+    prob = np_softmax(preds.logits.cpu().detach().numpy()[0])
+    out_dict = {n: p for n, p in zip(emotion_names_jp, prob)}
+
+    # 棒グラフを描画
+    if show_fig:
+        plt.figure(figsize=(8, 3))
+        df = pd.DataFrame(out_dict.items(), columns=['name', 'prob'])
+        sns.barplot(x='name', y='prob', data=df)
+        plt.title('入力文 : ' + text, fontsize=15)
+        plt.savefig("results/{}.png".format(text))
+
+    if ret_prob:
+        return out_dict
+
+
+# Plutchikの8つの基本感情
+emotion_names = ['Joy', 'Sadness', 'Anticipation', 'Surprise', 'Anger', 'Fear', 'Disgust', 'Trust']
+emotion_names_jp = ['喜び', '悲しみ', '期待', '驚き', '怒り', '恐れ', '嫌悪', '信頼']  # 日本語版
+num_labels = len(emotion_names)
+
+
+model_name = "kynea0b/cl-tohoku-bert-base-japanese-v3-wrime-8-emotions"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+
+# 動作確認
+analyze_emotion('今日から長期休暇だぁーーー！！！', show_fig=True)
+
+
+analyze_emotion('この書類にはコーヒーかかってなくて良かった…。不幸中の幸いだ。', show_fig=True)
+
+
+
+analyze_emotion('なんで自分だけこんな目に遭うんだ……', show_fig=True)
+
+
+analyze_emotion('君ならきっとやってくれると思っていたよ！', show_fig=True)
+
+
+analyze_emotion('え、今日って休校だったの？', show_fig=True)
+
+
+analyze_emotion('明日のプレゼンうまくできるかなぁ…', show_fig=True)
+
+
+
+analyze_emotion('あぁー、イライラするっ！！', show_fig=True)
+
+
+
+
